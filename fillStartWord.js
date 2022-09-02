2fab3aa8a49a86e802ff5b421e865cba47424526
WathanyuChomsri <wathanyu.chom@kmutt.ac.th>
2022-09-03 01:47:34 +0700

    .\fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,22 +1,23 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
-  if(startWord === "first"){
-     return ("firststep")
-  }else if (word === null){
+  
+ if (word == null){
     return (undefined)
-  } else if(word === undefined){
+  } else if(word == undefined){
     return (undefined)
-  } else{
+  } else if (startWord === word.substr(0,startWord.length) ){ //word.substr จุดเริ่มต้นของparameter word (ตำแหน่ง)ถ้าไปเท่ากับ,ตัวอักษรทั้งหมดของstartword จะทำการลบอักขระที่ซ้ำของ word ออก) --> string.substr(จุดเริ่มต้น, ความยาวที่ต้องการ)
+      return word
+  }else{
     return startWord + word
   }
 }
   
-// (fillStartWord("1-2565","Hello"))
-// (fillStartWord("JS","beginner"))
-// (fillStartWord("first","firststep"))
-// (fillStartWord("first",null))
-// (fillStartWord("first",undefined))
+// console.log(fillStartWord("1-2565","Hello"))
+// console.log(fillStartWord("JS","beginner"))
+// console.log(fillStartWord("first","firststep"))
+// console.log(fillStartWord("first",null))
+// console.log(fillStartWord("first",undefined))
 
-module.exports = fillStartWord
+//module.exports = fillStartWord
