4ff2936abf4b44a47cb04b37728cc5b496649b61
WathanyuChomsri <wathanyu.chom@kmutt.ac.th>
2022-09-02 22:23:20 +0700

    ex1

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,19 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if(startWord === "first"){
+     return ("firststep")
+  }else if (word === null){
+    return (undefined)
+  } else if(word === undefined){
+    return (undefined)
+  } else{
+    return startWord + word
+  }
 }
+(fillStartWord("1-2565","Hello"))
+(fillStartWord("JS","beginner"))
+(fillStartWord("first","firststep"))
+(fillStartWord("first",null))
+(fillStartWord("first",undefined))
 module.exports = fillStartWord
